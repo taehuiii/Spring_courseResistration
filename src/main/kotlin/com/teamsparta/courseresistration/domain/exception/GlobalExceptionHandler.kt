7caa432062ea564eaf3e4fc57daf1283c4222986
taehuiii <swanna18@naver.com>
2024-05-10 20:12:18 +0900

    [ADD] GlobalExceptionHandler

--- src/main/kotlin/com/teamsparta/courseresistration/domain/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/courseresistration/domain/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.teamsparta.courseresistration.domain.exception
 
+import com.teamsparta.courseresistration.domain.exception.dto.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
