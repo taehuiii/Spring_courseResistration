5510b5a1c8bc6e18f978db3b16c748de10df57cf
taehuiii <swanna18@naver.com>
2024-05-10 20:12:03 +0900

    [ADD] GlobalExceptionHandler

---
+++ src/main/kotlin/com/teamsparta/courseresistration/domain/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.teamsparta.courseresistration.domain.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(IllegalStateException::class)
+    fun handleIllegalStateException(e: IllegalStateException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponse(e.message))
+    }
+
+}
