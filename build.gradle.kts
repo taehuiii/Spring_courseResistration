cfc9197569e92833534d78a21f64d6552ce81fc8
taehuiii <swanna18@naver.com>
2024-05-12 17:54:38 +0900

    [ADD] DB연결 : postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.h2database:h2")
+   // implementation("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
